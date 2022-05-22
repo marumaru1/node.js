2ee6dd8ff0b747faf13f4ac5cf08c64d971e87f5
Maria Makliuk <annamary_1995@mail.ru>
2022-05-22 14:08:06 +0300

    feat:dd

---
+++ github-app/app.js
@@ -0,0 +1,8 @@
+const github = require('./github');
+const username = process.argv[2];
+github.getRepos(username, (error, repos) => {
+    if(error) return console.error(error.message);
+  
+    repos.forEach(repo => console.log(repo.name));
+  });
+  
---
+++ github-app/index.js
@@ -0,0 +1,3 @@
+require('colors');
+const text = 'Hello, world!';
+console.log(text.rainbow);
